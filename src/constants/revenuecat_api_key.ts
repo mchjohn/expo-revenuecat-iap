ed76482519f6cfde0bd63b82df491210251c597c
Michel John <michel.john@hotmail.com>
2024-01-27 19:36:46 -0300

    setup revenuecat

---
+++ src/constants/revenuecat_api_key.ts
@@ -0,0 +1,4 @@
+export const revenuecatAPIKeys = {
+  apple: "your_revenuecat_apple_api_key",
+  google: "goog_tAkyGdhhglcNHxquISXrItKRquX",
+};
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,37 +1,37 @@
 import { useEffect, useState } from 'react';
-import { View, FlatList, StyleSheet } from 'react-native';
+import { View, FlatList, StyleSheet, Platform } from 'react-native';
+import Purchases, { PurchasesOffering } from 'react-native-purchases';
+
+import { revenuecatAPIKeys } from '~/constants/revenuecat_api_key';
 
 import { BookCard } from '~/components/BookCard';
 import { Loading } from '~/components/Loading';
 import { IBook } from '~/interfaces';
-import { sleep } from '~/utils/sleep';
 
 export function Home() {
   const [books, setBooks] = useState<IBook[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentOffering, setCurrentOffering] = useState<PurchasesOffering | null>(null);
 
-  const getBooks = async () => {
-    setIsLoading(true);
-
-    await sleep();
-
-    await fetch('http://192.168.0.106:3000/books')
-      .then((response) => response.json())
-      .then((json) => {
-        setBooks(json);
-      })
-      .catch((error) => {
-        console.error(error);
-        console.error(JSON.stringify(error.message));
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
+  const setup = async () => {
+    if (Platform.OS == "android") {
+      await Purchases.configure({ apiKey: revenuecatAPIKeys.google });
+    } else {
+      await Purchases.configure({ apiKey: revenuecatAPIKeys.apple });
+    }
+    
+    const offerings = await Purchases.getOfferings();
+    setCurrentOffering(offerings.current);
   };
 
   useEffect(() => {
-    getBooks();
+    Purchases.setLogLevel(Purchases.LOG_LEVEL.DEBUG)
+
+    setup()
+    .catch(console.log);
   }, []);
+
+  console.debug({ currentOffering })
 
   return (
     <View style={styles.container}>
